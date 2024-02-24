beb05dd3d578e72027285df2163f44b91613a5d5
jeremy <jeremyninety@gmail.com>
2024-02-23 19:07:56 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jeremy Barnes-Smith on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
